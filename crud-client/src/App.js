c98ad4e05a052d242ed5e5508d5ca5486a2fc69c
dthemg <david.montgomery@live.se>
2020-03-30 22:19:15 +0200

    Added (some) material UI to make things look nicer

--- crud-client/src/App.js
+++ crud-client/src/App.js
@@ -1,13 +1,28 @@
 import React from 'react';
 import './App.css';
-import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
+import { withStyles, makeStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
-import TableContainer from '@material-ui/core/TableContainer';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import { spacing } from '@material-ui/system';
+
+const styleClasses = makeStyles(theme => ({
+	table: {
+		minWidth: 450,
+		minHeight: 400,
+		maxWidth: 750
+	},
+	root: {
+		'& > *': {
+      margin: theme.spacing(1),
+			width: '25ch',
+		},
+	}
+}));
 
 
 class App extends React.Component {
@@ -17,10 +32,10 @@ class App extends React.Component {
 			fruitResponse: [],
 			newName: "",
 			newWeight: "",
-			newRemoveId: -1,
-			newUpdateId: -1,
+			newRemoveId: "",
+			newUpdateId: "",
 			newUpdateName: "",
-			newUpdateWeight: -1
+			newUpdateWeight: ""
 		};
 		this.readAllFruits = this.readAllFruits.bind(this);
 		this.onSubmitNewFruit = this.onSubmitNewFruit.bind(this);
@@ -28,14 +43,6 @@ class App extends React.Component {
 		this.onSubmitUpdateId = this.onSubmitUpdateId.bind(this);
 		this.renderTable = this.renderTable.bind(this);
 		this.onChange = this.onChange.bind(this);
-	}
-
-	useStyles() {
-		return makeStyles({
-			table: {
-				minWidth: 650
-			}
-		});
 	}
 
 	readAllFruits() {
@@ -109,137 +116,163 @@ class App extends React.Component {
 	}
 
 	renderTable() {
-		const classes = this.useStyles();
-
+		const styleClasses = this.props.classes;
 		return (
-			<TableContainer component={Paper}>
-				<Table className={classes.table} aria-label="My table">
-					<TableHead>
-						<TableRow>
-							<TableCell>Name</TableCell>
-							<TableCell align='right'>Weight</TableCell>
-							<TableCell align='right'>Id</TableCell>
+			<Table className={styleClasses.table} aria-label="My table">
+				<TableHead>
+					<TableRow>
+						<TableCell>Name</TableCell>
+						<TableCell align='right'>Weight</TableCell>
+						<TableCell align='right'>Id</TableCell>
+					</TableRow>
+				</TableHead>
+				<TableBody>
+					{this.state.fruitResponse.map(rowContent => (
+						<TableRow key={rowContent.id}>
+							<TableCell component="th" scope="row">
+								{rowContent.name}
+							</TableCell>
+							<TableCell align='right'>{rowContent.weight}</TableCell>
+							<TableCell align='right'>{rowContent.id}</TableCell>
 						</TableRow>
-					</TableHead>
-					<TableBody>
-						{this.state.fruitResponse.map(rowContent => (
-							<TableRow key={rowContent.id}>
-								<TableCell component="th" scope="row">
-									{rowContent.name}
-								</TableCell>
-								<TableCell align='right'>{rowContent.weight}</TableCell>
-								<TableCell align='right'>{rowContent.id}</TableCell>
-							</TableRow>
-						))}
-					</TableBody>
-				</Table>
-			</TableContainer>
+					))}
+				</TableBody>
+			</Table>
 		);
 	}
-	
+
+	renderNewFruitForm() {
+		const styleClasses = this.props.classes;
+		
+		return (
+			<div>
+				<form className={styleClasses.root} autoComplete="off">
+					<div>
+						<TextField
+							label="Name"
+							name="newName"
+							value={this.state.newName}
+							onChange={this.onChange}
+						/>
+					</div>
+					<div>
+						<TextField
+							label="Weight"
+							name="newWeight"
+							value={this.state.newWeight}
+							onChange={this.onChange}
+						/>
+					</div>
+					<div>
+						<Button
+							onClick = { this.onSubmitNewFruit }
+							variant="contained"
+							color="primary"
+						>
+							Add to DB
+						</Button>
+
+					</div>
+				</form>
+			</div>
+		)
+	}
+
+	renderGetAll() {
+		return (
+			<div>
+				<Button
+					onClick = { this.readAllFruits }
+					variant="contained"
+					color="primary"
+				>
+					Get from DB
+				</Button>
+			</div>
+		)
+	}
+
+	renderRemove() {
+		return (
+			<div>
+				<div>
+					<TextField
+						label="Id"
+						name="newRemoveId"
+						value={this.state.newRemoveId}
+						onChange={this.onChange}
+					/>
+				</div>
+				<div>
+					<Button
+						onClick={ this.onSubmitRemoveId }
+						variant="contained"
+						color="primary"
+					>
+						Remove from DB
+					</Button>
+				</div>
+			</div>
+		)
+	}
+
+	renderUpdate() {
+		return (
+			<div>
+				<form>
+					<div>
+						<TextField
+							label="New name"
+							name="newUpdateName"
+							value={this.state.newUpdateName}
+							onChange={this.onChange}
+						/>
+					</div>
+					<div>
+						<TextField
+							label="New weight"
+							name="newUpdateWeight"
+							value={this.state.newUpdateWeight}
+							onChange={this.onChange}
+						/>
+					</div>
+					<div>
+						<TextField
+							label="Id"
+							name="newUpdateId"
+							value={this.state.newUpdateId}
+							onChange={this.onChange}
+						/>
+					</div>
+					<div>
+						<Button
+							onClick={ this.onSubmitUpdateId }
+							variant="contained"
+							color="primary"
+						>
+							Update DB
+						</Button>
+					</div>
+				</form>
+			</div>
+		)
+	}
 
 	render() {
 		return (
 			<div>
+				<h2>Create</h2>
+				{ this.renderNewFruitForm() }
+				<h2>Read</h2>
+				{ this.renderGetAll() }
 				{ this.renderTable() }
-				<div>
-					<button
-						onClick = { this.readAllFruits }
-					>
-						Get DB content
-					</button>
-				</div>
-				<div>
-					<form>
-						<div>
-							<label>Name: </label>
-							<input
-								type="text"
-								id="name"
-								name="newName"
-								value={this.state.newName}
-								onChange={this.onChange}
-							/>
-						</div>
-						<div>
-							<label>Weight: </label>
-							<input
-								type="int"
-								id="weight"
-								name="newWeight"
-								value={this.state.newWeight}
-								onChange={this.onChange}
-							/>
-						</div>
-						<div>
-							<input type="submit"
-								onClick = { this.onSubmitNewFruit }
-								value="Submit new fruit"
-							/>
-						</div>
-					</form>
-				</div>
-				<div>
-					<div>
-						<p>Remove fruit with id</p>
-					</div>
-					<form>
-						<label>Id: </label>
-						<input
-							type="int"
-							id="removeId"
-							name="newRemoveId"
-							value={this.state.newRemoveId}
-							onChange={this.onChange}
-						/>
-						<input
-							type="submit"
-							onClick={ this.onSubmitRemoveId }
-							value="Remove specified id"
-						/>
-					</form>
-				</div>
-				<div>
-					<p>Update fruit with id</p>
-					<form>
-						<div>
-							<label>New name: </label>
-							<input
-								type="text"
-								name="newUpdateName"
-								value={this.state.newUpdateName}
-								onChange={this.onChange}
-							/>
-						</div>
-						<div>
-							<label>New weight: </label>
-							<input
-								type="int"
-								name="newUpdateWeight"
-								value={this.state.newUpdateWeight}
-								onChange={this.onChange}
-							/>
-						</div>
-						<div>
-							<label>Id: </label>
-							<input
-								type="int"
-								id="updateId"
-								name="newUpdateId"
-								value={this.state.newUpdateId}
-								onChange={this.onChange}
-							/>
-							<input
-								type="submit"
-								onClick={ this.onSubmitUpdateId }
-								value="Update specified id"
-							/>
-						</div>
-					</form>
-				</div>
+				<h2>Update</h2>
+				{ this.renderUpdate() }
+				<h2>Remove</h2>
+				{ this.renderRemove() }
 			</div>
 		);
 	}
 }
 
-export default App;
+export default withStyles(styleClasses)(App);
